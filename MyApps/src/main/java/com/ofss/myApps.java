1cecb487439e4640ce67599fc65b0fea5831cee5
brajnet <braj.net@gmail.com>
2020-06-24 11:24:41 +0530

    Commiting myApps.java

---
+++ MyApps/src/main/java/com/ofss/myApps.java
@@ -0,0 +1,10 @@
+package com.ofss;
+
+public class myApps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome to Eclipse Project Maven Integration with Githum");
+	}
+
+}
